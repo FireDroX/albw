662fd72ca4f053628c5aea6d0cb7f2e8667b64c9
FireDroX <83990394+FireDroX@users.noreply.github.com>
2024-05-12 20:33:04 +0200

    Mass Update 🎳 : v0.4.0 support

--- src/components/ReadSeed/ReadSeed.jsx
+++ src/components/ReadSeed/ReadSeed.jsx
@@ -6,9 +6,15 @@ import { items, itemsImg } from "../../utils/items";
 import { RunContext } from "../../utils/RunContext";
 
 const ReadSeed = ({ data, reset = function () {}, where = "reader" }) => {
-  const { layout, seed, version } = data;
+  const {
+    layout,
+    seed,
+    version = "????",
+    portal_map = undefined,
+    weather_vane_map = undefined,
+  } = data;
   const { setIsClicked } = useContext(RunContext);
-  const [itemIndex, setItemIndex] = useState(0);
+  const [itemIndex, setItemIndex] = useState({ type: 0, index: 0 });
 
   const navigate = useNavigate();
 
@@ -18,29 +24,61 @@ const ReadSeed = ({ data, reset = function () {}, where = "reader" }) => {
     setIsClicked({ clicked: false, index: 0 });
   };
 
-  function findLocationOfItemsInLayout(itemsName) {
+  const allLocationsOfItems = () => {
     const locations = [];
-    itemsName.map((item, index) => {
-      for (const world in layout) {
-        for (const zone in layout[world]) {
-          for (const location in layout[world][zone]) {
-            if (layout[world][zone][location] === item)
-              locations.push(
+    items.map((types, typeIndex) => {
+      locations.push([]);
+      types.map((itemArray, itemIndex) => {
+        locations[typeIndex].push([]);
+        itemArray.map((item) => {
+          for (const world in layout) {
+            for (const zone in layout[world]) {
+              for (const location in layout[world][zone]) {
+                if (layout[world][zone][location] === item)
+                  locations[typeIndex][itemIndex].push(
+                    <>
+                      <span className="location-item">{item}</span>
+                      {" in "}
+                      <span className="location-world">{world}</span>
+                      {" at "}
+                      <span className="location-info">{`${zone} - ${location}`}</span>
+                    </>
+                  );
+              }
+            }
+          }
+          if (item === "%portal_map%") {
+            for (const portal in portal_map) {
+              locations[typeIndex][itemIndex].push(
                 <>
-                  <span className="location-item">{item}</span>
-                  {" in "}
-                  <span className="location-world">{world}</span>
+                  <span className="location-item">{portal_map[portal]}</span>
                   {" at "}
-                  <span className="location-info">{`${zone} - ${location}`}</span>
+                  <span className="location-info">{portal}</span>
                 </>
               );
+            }
           }
-        }
-      }
-      return true;
+          if (item === "%weather_vane_map%") {
+            for (const weather_vane in weather_vane_map) {
+              locations[typeIndex][itemIndex].push(
+                <>
+                  <span className="location-item">
+                    {weather_vane_map[weather_vane]}
+                  </span>
+                  {" at "}
+                  <span className="location-info">{weather_vane}</span>
+                </>
+              );
+            }
+          }
+          return 0;
+        });
+        return 0;
+      });
+      return 0;
     });
     return locations;
-  }
+  };
 
   return (
     <div className="read-container">
@@ -51,24 +89,44 @@ const ReadSeed = ({ data, reset = function () {}, where = "reader" }) => {
             <span className="location-info">{version}</span>
           </small>
         </div>
-        <div className="read-imgs">
-          {itemsImg.map((img, i) => (
-            <img
-              alt={"ICON_" + i}
-              src={img}
-              key={img + i}
-              style={{
-                backgroundColor:
-                  itemIndex === i ? "var(--secondary35)" : "transparent",
-              }}
-              onClick={() => setItemIndex(i)}
-            />
-          ))}
+        <div className="read-imgs-container">
+          {allLocationsOfItems().map((types, typesIndex) =>
+            types[typesIndex].length > 0 ? (
+              <div className="read-imgs-sub" key={typesIndex}>
+                <h6>{["ITEMS", "OTHERS", "KEYS"][typesIndex]} :</h6>
+                <div className="read-imgs">
+                  {types.map((items, itemsIndex) =>
+                    items.length > 0 ? (
+                      <img
+                        alt={"ICON_" + typesIndex + "-" + itemsIndex}
+                        src={itemsImg[typesIndex][itemsIndex]}
+                        key={typesIndex + "-" + itemsIndex}
+                        style={{
+                          backgroundColor:
+                            itemIndex.type === typesIndex &&
+                            itemIndex.index === itemsIndex
+                              ? "var(--secondary35)"
+                              : "transparent",
+                        }}
+                        onClick={() =>
+                          setItemIndex({ type: typesIndex, index: itemsIndex })
+                        }
+                      />
+                    ) : undefined
+                  )}
+                </div>
+              </div>
+            ) : (
+              false
+            )
+          )}
         </div>
         <div className="read-items">
-          {findLocationOfItemsInLayout(items[itemIndex]).map((el, i) => (
-            <small key={el + i}>{el}</small>
-          ))}
+          {allLocationsOfItems()[itemIndex.type][itemIndex.index].map(
+            (el, i) => (
+              <small key={el + i}>{el}</small>
+            )
+          )}
         </div>
       </div>
       <small className="read-goback" onClick={handleNavigate}>
